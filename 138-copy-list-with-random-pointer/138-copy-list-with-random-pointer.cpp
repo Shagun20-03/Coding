2b0555a8a24c5fc844eb3804034e3e3d8bf4cc83
Shagun Goyal <goyalshagun03@gmail.com>
2022-09-25 00:35:07 +0530

    Time: 12 ms (70.43%), Space: 11.4 MB (27.87%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,19 +17,55 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        map<Node*, Node*> m;
-        int i=0;
-        Node* ptr = head;
-        while (ptr) {
-            m[ptr] =new Node(ptr->val);
-            ptr = ptr->next;
+        Node* iter=head;
+        
+        // First round: make copy of each node,
+  // and link them together side-by-side in a single list.
+        while(iter!=NULL)
+        {
+            Node* copy=iter->next;
+            iter->next=new Node(iter->val);
+            iter->next->next=copy;
+            iter=copy;
         }
-        ptr = head;
-        while (ptr) {
-            m[ptr]->next = m[ptr->next];
-            m[ptr]->random = m[ptr->random];
-            ptr = ptr->next;
+         // Second round: assign random pointers for the copy nodes.
+        iter=head;
+        while(iter!=NULL)
+        {
+            if(iter->random!=NULL){
+                iter->next->random=iter->random->next;
+            }
+            iter=iter->next->next;       
         }
-        return m[head];
+         // Third round: restore the original list, and extract the copy list.
+    
+        Node* pseudoHead=new Node(0);
+        Node* copyIter=pseudoHead;
+        
+        while(head!=NULL)
+        {
+            copyIter->next=head->next;
+            copyIter=copyIter->next;
+            
+            head->next = head->next->next;
+            head=head->next;
+        }
+        return pseudoHead->next;
+        
+        //2nd approach
+        // map<Node*, Node*> m;
+        // int i=0;
+        // Node* ptr = head;
+        // while (ptr) {
+        //     m[ptr] =new Node(ptr->val);
+        //     ptr = ptr->next;
+        // }
+        // ptr = head;
+        // while (ptr) {
+        //     m[ptr]->next = m[ptr->next];
+        //     m[ptr]->random = m[ptr->random];
+        //     ptr = ptr->next;
+        // }
+        // return m[head];
     }
 };
