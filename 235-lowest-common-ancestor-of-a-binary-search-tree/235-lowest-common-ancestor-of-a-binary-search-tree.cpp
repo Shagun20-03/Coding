611866265912356471a384709e3e528124670244
Shagun Goyal <goyalshagun03@gmail.com>
2022-09-27 10:52:01 +0530

    Time: 36 ms (79.00%), Space: 23.4 MB (15.09%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,15 +11,31 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL)
-            return NULL;
-        int curr=root->val;
-        if(curr>p->val && curr>q->val){
-            return lowestCommonAncestor(root->left, p, q);
-        }
-        if(curr<p->val && curr<q->val){
-            return lowestCommonAncestor(root->right, p, q);
-        }
-        return root;
+        // if(root==NULL)
+        //     return NULL;
+        // int curr=root->val;
+        // if(curr>p->val && curr>q->val){
+        //     return lowestCommonAncestor(root->left, p, q);
+        // }
+        // if(curr<p->val && curr<q->val){
+        //     return lowestCommonAncestor(root->right, p, q);
+        // }
+        // return root;
+        
+        while (root != NULL) {
+        // If both n1 and n2 are smaller than root,
+        // then LCA lies in left
+        if (root->val > p->val && root->val > q->val)
+            root = root->left;
+ 
+        // If both n1 and n2 are greater than root,
+        // then LCA lies in right
+        else if (root->val < p->val && root->val < q->val)
+            root = root->right;
+ 
+        else
+            break;
+    }
+    return root;
     }
 };
