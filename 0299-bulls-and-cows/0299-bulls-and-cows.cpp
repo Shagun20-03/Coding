d2958dd63007ee6af7e4006cf65b8a0839088869
Shagun Goyal <goyalshagun03@gmail.com>
2023-01-04 19:40:45 +0530

    Time: 10 ms (34.42%), Space: 6.6 MB (43.67%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     string getHint(string secret, string guess) {
         int bulls=0, cows=0;
+        vector<int> s(10, 0);
+        vector<int> g(10, 0);
+        //int nums[10]={0};
         
-        int nums[10]={0};
         for(int i=0;i<secret.length();i++){
-            int s=secret[i]-'0';
-            int g=guess[i]-'0';
-            if(s==g){
+            // int s=secret[i]-'0';
+            // int g=guess[i]-'0';
+            if(secret[i]==guess[i]){
                 bulls++;
             }
             else{
-                if(nums[s]++ < 0) cows++;
-                if(nums[g]-- > 0) cows++;
+                s[secret[i]-'0']++;
+                g[guess[i]-'0']++;
             }
+        }
+        for(int i=0;i<10;i++){
+            cows+=min(s[i], g[i]);
         }
         return to_string(bulls)+"A"+to_string(cows)+"B";
     }
