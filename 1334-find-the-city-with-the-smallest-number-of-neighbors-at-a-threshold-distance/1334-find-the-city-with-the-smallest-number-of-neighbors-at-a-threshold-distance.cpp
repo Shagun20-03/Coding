2a559d6dbe96ff0b5e2035711890a4a0e2ed4f71
Shagun Goyal <goyalshagun03@gmail.com>
2023-01-14 19:29:47 +0530

    Time: 76 ms (93.31%), Space: 13.5 MB (25.62%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,94 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        //solved using dijkstra and by using foyd warshall done in gfg
+//         vector<pair<int,int>> adj[n];
+//         for(auto it:edges){
+//             adj[it[0]].push_back({it[1], it[2]});
+//             adj[it[1]].push_back({it[0], it[2]});
+//         }
+//         priority_queue<pair<int, int>,vector<pair<int, int>>, greater<pair<int, int>>>pq;
+//         int cntCity=1e9;
+//         int cityNo;
+        
+//         for(int i=0;i<n;i++) 
+//         {
+//             vector<int> dist(n, 1e9);
+//             pq.push({0, i});
+//             dist[i]=0;
+//             while(!pq.empty()) 
+//             {
+//                 int dis=pq.top().first;
+//                 int node=pq.top().second;
+//                 pq.pop();
+//                 for(auto it:adj[node]) 
+//                 {
+//                     int adjNode=it.first;
+//                     int adjWeight=it.second;
+//                     if(dis+adjWeight<dist[adjNode]) 
+//                     {
+//                         dist[adjNode]=dis+adjWeight;
+//                         pq.push({dist[adjNode],adjNode});
+//                     }
+//                 }
+//             }
+//             int count=0;
+//             for(int j=0;j<n;i++)
+//             {
+//                 if(dist[j]<=distanceThreshold)
+//                     count++;
+//             }
+//             if(count<=cntCity)
+//             {
+//                 cntCity=count;
+//                 cityNo=i;
+//             }
+//         }
+//         return cityNo;
+         vector<pair<int,int>> adj[n];
+        for(auto it:edges)
+        {
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
+        }
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        
+        int cityno,mincitycount=1e9;
+        
+        for(int i=0;i<n;i++)
+        {
+            vector<int> dist(n,1e9);
+            pq.push({0,i});
+            dist[i]=0;
+            while(!pq.empty())
+            {
+                int distance=pq.top().first;
+                int node=pq.top().second;
+                pq.pop();
+                for(auto it:adj[node])
+                {
+                    int adjNode=it.first;
+                    int adjWeight=it.second;
+                    if(distance + adjWeight < dist[adjNode])
+                    {
+                        dist[adjNode] = distance + adjWeight;
+                        pq.push({dist[adjNode],adjNode});
+                    }
+                }
+            }
+            int count=0;
+            for(int j=0;j<n;j++)
+            {
+                if(dist[j]<=distanceThreshold)
+                    count++;
+            }
+            if(count<=mincitycount)
+            {
+                mincitycount=count;
+                cityno = i;
+            }
+        }
+        return cityno;
+    }
+};
