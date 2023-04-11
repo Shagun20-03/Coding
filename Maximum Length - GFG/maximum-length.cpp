43a51eda51b2e9c126e46c4d6549ee9da6c06531
Shagun Goyal <goyalshagun03@gmail.com>
2023-04-12 00:01:04 +0530

    Added solution - LeetHub

---
+++ Maximum Length - GFG/maximum-length.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    int solve(int a, int b, int c) {
+        // code here
+        int maxi = max({a,b,c});
+      int sum = a+b+c;
+      sum-=maxi;
+      if(maxi>2*(sum+1)){
+          return -1;
+      }
+      else{
+          return a+b+c;
+    }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        Solution ob;
+        int ans = ob.solve(a, b, c);
+     
+
+        cout <<  ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
