650f0002e4941560e11675fbd33069ded62d5983
Shagun Goyal <goyalshagun03@gmail.com>
2023-04-16 21:23:00 +0530

    Time: 338 ms (46.12%), Space: 53.4 MB (50.86%) - LeetHub

---
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    long long mod=1e9+7;
+    int solve(vector<vector<int>> &freq, string &target, vector<vector<int>> &dp, int colIdx, int targetIdx){
+        if(targetIdx==target.size())
+            return 1;
+        if(colIdx==freq[0].size())
+            return 0;
+        if(dp[colIdx][targetIdx]!=-1)
+            return dp[colIdx][targetIdx];
+        
+        int targetCh=target[targetIdx]-'a';
+        int notPick=solve(freq, target, dp, colIdx+1, targetIdx);
+        
+        int pick=0;
+        if(freq[targetCh][colIdx]!=0){
+            int currWays=freq[targetCh][colIdx];
+            int nextWays=solve(freq, target, dp, colIdx+1, targetIdx+1);
+            pick=(currWays%mod * nextWays%mod)%mod;
+        }
+        return dp[colIdx][targetIdx]=(pick%mod + notPick%mod)%mod;
+    }
+    int numWays(vector<string>& words, string target) {
+        int m=words.size(), n=words[0].size();
+        vector<vector<int>> freq(26, vector<int>(n, 0));
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                freq[words[i][j]-'a'][j]++;
+            }
+        }
+        vector<vector<int>> dp(n, vector<int>(target.size(), -1));
+        int ans=solve(freq, target, dp, 0, 0);
+        return ans;
+    }
+};
