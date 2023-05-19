eaf189de4072f6cc45fab38f8abb55a2bfdab381
Shagun Goyal <goyalshagun03@gmail.com>
2023-05-20 04:04:15 +0530

    Time: 27 ms (56.03%), Space: 13.7 MB (41.47%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -6,29 +6,31 @@ class Solution {
         q.push(start);
         //starting the color of node with 0
         color[start]=0;
-        while(!q.empty()){
+        while(!q.empty())
+        {
             int node=q.front();
             q.pop();
             
-            for(auto it: graph[node]){
+            for(auto it: graph[node]) {
                 if(color[it]==-1){
                     //color the adj node with opposite color of current node
                     color[it]=!color[node];
                     q.push(it);
                 }
                 //already colored with the same color of the current node
-                else if(color[it]==color[node]){
+                else if(color[it]==color[node]) {
                     return false;
                 }
             }
         }
+        //if we can traverse and color through the end of that component it is bipartite
         return true;
     }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int V=graph.size();
         int color[V];
-        for(int i=0;i<V;i++){
+        for(int i=0;i<V;i++) {
             color[i]=-1;
         }
         //checking for all components
