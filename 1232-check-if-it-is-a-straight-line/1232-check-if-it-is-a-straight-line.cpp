c1c1f79b0e13099167be13b30ea1a77a9e48cb64
Shagun Goyal <goyalshagun03@gmail.com>
2023-06-05 23:49:30 +0530

    Time: 8 ms (73.14%), Space: 10.1 MB (63.27%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getXDiff(vector<int> &a, vector<int> &b){
+        return a[0]-b[0];
+    }
+    int getYDiff(vector<int> &a, vector<int> &b){
+        return a[1]-b[1];
+    }
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int deltaX=getXDiff(coordinates[1], coordinates[0]);
+        int deltaY=getYDiff(coordinates[1], coordinates[0]);
+        
+        for(int i=2;i<coordinates.size();i++){
+            if(deltaX*getYDiff(coordinates[i], coordinates[0])!=deltaY*getXDiff(coordinates[i], coordinates[0]))
+                return false;
+        }
+        return true;
+    }
+};
