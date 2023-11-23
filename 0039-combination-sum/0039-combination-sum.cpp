d569fa0c8d4dd9fa36168d665bc45c897130c901
Shagun Goyal <goyalshagun03@gmail.com>
2023-11-23 17:50:35 +0530

    Time: 7 ms (52.63%), Space: 11 MB (97.93%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,22 @@
 class Solution {
-    private:
-    void findCombination(int i, int target, vector<int>& candidates, vector<vector<int>> &ans, vector<int> &v){
-        //base cond
-        if(i==candidates.size()){
-            if(target==0){
-                ans.push_back(v);
-            }
-            return; //no further recursion will take place
-        }
-        //pick the element
-        if(candidates[i]<=target) {
-            v.push_back(candidates[i]);
-            //find for that index only 
-            findCombination(i, target-candidates[i], candidates, ans, v);
-            v.pop_back();   //pop out that element for not pick condition
-        }
-        //we decide to move further with next index value 
-        findCombination(i+1, target, candidates, ans, v);
-    }
 public:
+    void findCombination(int ind, int target, vector<int>& candidates, vector<vector<int>> &ans,  vector<int> &temp){
+        if(ind==candidates.size()){
+            if(target==0)
+                ans.push_back(temp);
+            return;
+        }
+        if(candidates[ind]<=target){
+            temp.push_back(candidates[ind]);
+            findCombination(ind, target-candidates[ind], candidates, ans, temp);
+            temp.pop_back();    //backtrak
+        }
+        findCombination(ind+1, target, candidates, ans, temp);
+    }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
-        vector<int> v;
-        
-        findCombination(0, target, candidates, ans, v);
+        vector<int> temp;
+        findCombination(0, target, candidates, ans, temp);
         return ans;
     }
 };
